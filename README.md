464f1aef59dfc2f00de16553e279962bb8c12bcf
largentba21 <largentba21@gmail.com>
2021-11-21 10:40:03 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Foundry VTT Expanded Status Effects
+
+A module that adds additional status effects to the TokenHUD.
+
+## Added Status Effects
+
+### DnD5e Conditions
+
+- Blinded
+- Charmed
+- Concentrating
+- Deafened
+- Exhaustion 1-5
+- Frightened
+- Grappled
+- Incapacitated
+- Inspiration
+- Invisible
+- Paralyzed
+- Petrified
+- Poisoned
+- Prone
+- Restrained
+- Stunned
+- Unconscious
+
+### DnD5e Misc Spell/Ability Effects
+
+- Aid
+- Bless
+- Bane
+- Rage
+- Haste
+- Hex
+- Hexblade's Curse
+- Hunter's Mark
+- Bardic Inspiration
+- Patient Defense
---
+++ scripts/index.js
@@ -0,0 +1,159 @@
+Hooks.once("init", () => {
+  CONFIG.statusEffects.push(
+    {
+      label: "Blinded",
+      icon: "modules/foundry-vtt-expanded-status-effects/icons/blinded.svg",
+      id: "Blinded",
+    },
+    {
+      label: "Charmed",
+      icon: "modules/foundry-vtt-expanded-status-effects/icons/charmed.svg",
+      id: "Charmed",
+    },
+    {
+      label: "Concentrating",
+      icon: "modules/foundry-vtt-expanded-status-effects/icons/concentrating.svg",
+      id: "Concentrating",
+    },
+    {
+      label: "Deafened",
+      icon: "modules/foundry-vtt-expanded-status-effects/icons/deafened.svg",
+      id: "Deafened",
+    },
+    {
+      label: "Exhaustion 1",
+      icon: "modules/foundry-vtt-expanded-status-effects/icons/exhaustion1.svg",
+      id: "Exhaustion",
+    },
+    {
+      label: "Exhaustion 2",
+      icon: "modules/foundry-vtt-expanded-status-effects/icons/exhaustion2.svg",
+      id: "Exhaustion",
+    },
+    {
+      label: "Exhaustion 3",
+      icon: "modules/foundry-vtt-expanded-status-effects/icons/exhaustion3.svg",
+      id: "Exhaustion",
+    },
+    {
+      label: "Exhaustion 4",
+      icon: "modules/foundry-vtt-expanded-status-effects/icons/exhaustion4.svg",
+      id: "Exhaustion",
+    },
+    {
+      label: "Exhaustion 5",
+      icon: "modules/foundry-vtt-expanded-status-effects/icons/exhaustion5.svg",
+      id: "Exhaustion",
+    },
+    {
+      label: "Frightened",
+      icon: "modules/foundry-vtt-expanded-status-effects/icons/frightened.svg",
+      id: "Frightened",
+    },
+    {
+      label: "Grappled",
+      icon: "modules/foundry-vtt-expanded-status-effects/icons/grappled.svg",
+      id: "Grappled",
+    },
+    {
+      label: "Incapacitated",
+      icon: "modules/foundry-vtt-expanded-status-effects/icons/incapacitated.svg",
+      id: "Incapacitated",
+    },
+    {
+      label: "Inspiration",
+      icon: "modules/foundry-vtt-expanded-status-effects/icons/inspiration.svg",
+      id: "Inspiration",
+    },
+    {
+      label: "Invisible",
+      icon: "modules/foundry-vtt-expanded-status-effects/icons/invisible.svg",
+      id: "Invisible",
+    },
+    {
+      label: "Paralyzed",
+      icon: "modules/foundry-vtt-expanded-status-effects/icons/paralyzed.svg",
+      id: "Paralyzed",
+    },
+    {
+      label: "Petrified",
+      icon: "modules/foundry-vtt-expanded-status-effects/icons/petrified.svg",
+      id: "Petrified",
+    },
+    {
+      label: "Poisoned",
+      icon: "modules/foundry-vtt-expanded-status-effects/icons/poisoned.svg",
+      id: "Poisoned",
+    },
+    {
+      label: "Prone",
+      icon: "modules/foundry-vtt-expanded-status-effects/icons/prone.svg",
+      id: "Prone",
+    },
+    {
+      label: "Restrained",
+      icon: "modules/foundry-vtt-expanded-status-effects/icons/restrained.svg",
+      id: "Restrained",
+    },
+    {
+      label: "Stunned",
+      icon: "modules/foundry-vtt-expanded-status-effects/icons/stunned.svg",
+      id: "Stunned",
+    },
+    {
+      label: "Unconscious",
+      icon: "icons/svg/unconscious.svg",
+      id: "Unconscious",
+    },
+    {
+      label: "Aid",
+      icon: "systems/dnd5e/icons/spells/heal-sky-1.jpg",
+      id: "Aid",
+    },
+    {
+      label: "Bless",
+      icon: "systems/dnd5e/icons/spells/haste-sky-1.jpg",
+      id: "Bless",
+    },
+    {
+      label: "Bane",
+      icon: "systems/dnd5e/icons/spells/rip-magenta-2.jpg",
+      id: "Bane",
+    },
+    {
+      label: "Rage",
+      icon: "systems/dnd5e/icons/skills/red_10.jpg",
+      id: "Rage",
+    },
+    {
+      label: "Haste",
+      icon: "systems/dnd5e/icons/spells/haste-royal-2.jpg",
+      id: "Haste",
+    },
+    {
+      label: "Hex",
+      icon: "systems/dnd5e/icons/spells/evil-eye-eerie-1.jpg",
+      id: "Hex",
+    },
+    {
+      label: "Hexblade's Curse",
+      icon: "systems/dnd5e/icons/spells/enchant-eerie-2.jpg",
+      id: "Hexblade",
+    },
+    {
+      label: "Hunter's Mark",
+      icon: "systems/dnd5e/icons/spells/evil-eye-red-1.jpg",
+      id: "Hunter",
+    },
+    {
+      label: "Bardic Inspiration",
+      icon: "systems/dnd5e/icons/skills/yellow_08.jpg",
+      id: "Bardic",
+    },
+    {
+      label: "Patient Defense",
+      icon: "systems/dnd5e/icons/skills/blue_18.jpg",
+      id: "Patient",
+    }
+  );
+});
